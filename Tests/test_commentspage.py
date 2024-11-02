1776ece4156361dff55fd29084b1f38da510e9b8
sanjana1399 <57448690+sanjana1399@users.noreply.github.com>
2024-11-01 23:13:40 -0400

    Update test_commentspage.py - basic tests for comments page

--- Tests/test_commentspage.py
+++ Tests/test_commentspage.py
@@ -1,1 +1,14 @@
+import unittest
+from unittest.mock import patch
+from app import app
 
+class TestLeaveReview(unittest.TestCase):
+    
+    def setUp(self):
+        self.client = app.test_client()
+        self.client.testing = True
+
+    def test_leave_review_route(self):
+        response = self.client.get('/student/leave_review')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h1>WolfTrack 6.0</h1>', response.data)
