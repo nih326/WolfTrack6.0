d949ca92fa68ced50500aaf2cc23a1cfd68c5233
sanjana1399 <57448690+sanjana1399@users.noreply.github.com>
2024-11-01 22:58:56 -0400

    Update test_networking_contacts.py - new tests

--- Tests/test_networking_contacts.py
+++ Tests/test_networking_contacts.py
@@ -1,1 +1,14 @@
+import unittest
+from unittest.mock import patch
+from app import app
 
+class TestNetworkingContacts(unittest.TestCase):
+    
+    def setUp(self):
+        self.client = app.test_client()
+        self.client.testing = True
+
+    def test_networking_contacts_route(self):
+        response = self.client.get('/student/networking_contacts')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h1>Networking Contacts</h1>', response.data)
