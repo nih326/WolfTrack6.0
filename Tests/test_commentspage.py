6f0487eba9b2b9d1431b54bb6eef89018e46f657
sanjana1399 <57448690+sanjana1399@users.noreply.github.com>
2024-11-01 23:17:06 -0400

    Update test_commentspage.py - Additional Tests

--- Tests/test_commentspage.py
+++ Tests/test_commentspage.py
@@ -12,3 +12,12 @@ class TestLeaveReview(unittest.TestCase):
         response = self.client.get('/student/leave_review')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<h1>WolfTrack 6.0</h1>', response.data)
+
+    def test_leave_review_html_structure(self):
+        response = self.client.get('/student/leave_review')
+        self.assertIn(b'<form id="commentForm">', response.data)
+        self.assertIn(b'<input type="text" id="nameInput"', response.data)
+        self.assertIn(b'<textarea id="commentInput"', response.data)
+        self.assertIn(b'<div class="stars" id="starRating">', response.data)
+        self.assertIn(b'<button type="submit">Submit</button>', response.data)
+        self.assertIn(b'<button id="clearComments">Clear All Comments</button>', response.data)
